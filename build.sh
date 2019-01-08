94ed232cea7ed2c5b5ae30e7e2b791edf8ecb736
hejie <hejiejack@didichuxing.com>
2019-01-08 17:26:17 +0800

    增加下载页

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cml build
-cp ./app-release.apk ./dist/
+cp ./res/* ./dist/
 #scp -r ./dist/weex/* ./dist/web/* xiaoju@10.179.100.193:/home/xiaoju/webroot/
 
+# 使用didiup工具上传文件到线上cml-playground文件夹，didiup使用说明查看http://npm.xiaojukeji.com/package/@didi/didi-uploader
 didiup -d ./dist -r '**/*.*'  -c cml-playground
