80ff87ca302a0ac5850491f0e2b5210945281579
hejie <hejiejack@didichuxing.com>
2019-01-08 15:49:46 +0800

    添加playgrounplayground

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cml build
-
+cp ./app-release.apk ./dist/
 #scp -r ./dist/weex/* ./dist/web/* xiaoju@10.179.100.193:/home/xiaoju/webroot/
 
 didiup -d ./dist -r '**/*.*'  -c cml-playground
