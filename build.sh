359b2fb15bf56cbd50d7b9657d1f25bd9259dead
hejiejack <hejiejack@didichuxing.com>
2019-07-31 15:46:33 +0800

    update

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cp ./res/* ./dist/
 #scp -r ./dist/weex/* ./dist/web/* xiaoju@10.179.100.193:/home/xiaoju/webroot/
 
 # 使用didiup工具上传文件到线上cml-playground文件夹，didiup使用说明查看http://npm.xiaojukeji.com/package/@didi/didi-uploader
-didiup -d ./dist -r '**/*.*'  -c cml-playground
+# didiup -d ./dist -r '**/*.*'  -c cml-playground
